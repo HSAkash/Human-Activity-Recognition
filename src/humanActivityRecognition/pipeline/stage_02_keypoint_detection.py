6567089f1257406a77aef370b79d892942f46f99
HSAkash <hemelakash472@gmail.com>
2024-10-25 11:32:14 +0600

    rename the name of the classes and valiables

--- src/humanActivityRecognition/pipeline/stage_02_keypoint_detection.py
+++ src/humanActivityRecognition/pipeline/stage_02_keypoint_detection.py
@@ -5,15 +5,15 @@ from humanActivityRecognition import logger
 
 STAGE_NAME = "Keypoint Detection"
 
-class DataIngestionTrainingPipeline:
+class KeypointDetectionPipeline:
     def __init__(self):
         pass
 
     def main(self):
         config = ConfigurationManager()
-        image_extraction_config = config.get_keypoint_detection_config()
-        image_extraction = KeypointDetection(config=image_extraction_config)
-        image_extraction.run()
+        keypoint_detection_config = config.get_keypoint_detection_config()
+        keypoint_detection = KeypointDetection(config=keypoint_detection_config)
+        keypoint_detection.run()
 
 
 
@@ -21,7 +21,7 @@ class DataIngestionTrainingPipeline:
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
+        obj = KeypointDetectionPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
--- src/humanActivityRecognition/pipeline/stage_03_bluring_image.py
+++ src/humanActivityRecognition/pipeline/stage_03_bluring_image.py
@@ -5,15 +5,15 @@ from humanActivityRecognition import logger
 
 STAGE_NAME = "Bluring The Images"
 
-class DataIngestionTrainingPipeline:
+class BluringImagePipeline:
     def __init__(self):
         pass
 
     def main(self):
         config = ConfigurationManager()
-        image_extraction_config = config.get_bluring_image_config()
-        image_extraction = BluringImage(config=image_extraction_config)
-        image_extraction.run()
+        bluring_image_config = config.get_bluring_image_config()
+        bluring_image = BluringImage(config=bluring_image_config)
+        bluring_image.run()
 
 
 
@@ -21,7 +21,7 @@ class DataIngestionTrainingPipeline:
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
+        obj = BluringImagePipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
